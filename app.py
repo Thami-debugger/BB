f54e53cc43956eb672011aa3cc3ab9d0ba945871
Thami-debugger <thamithami816@gmail.com>
2025-05-26 20:12:04 +0200

    Fixed Render port issue

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def index():
     return render_template('index.html', friday_price=friday_price)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
